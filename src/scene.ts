e7f5b0214172a670f55af3aff84316abdbaded7f
Yixi <lyh1112@gmail.com>
2020-01-05 23:36:01 +0800

    fix build issue

--- src/scene.ts
+++ src/scene.ts
@@ -4,11 +4,18 @@ export const createScene = (engine: BABYLON.Engine, canvas: HTMLCanvasElement) =
   const scene = new BABYLON.Scene(engine)
 
   const ground = BABYLON.MeshBuilder.CreateGround('ground', {height: 100, width: 100, subdivisions: 9}, scene)
-  let groundMaterial = new BABYLON.StandardMaterial('ground-material', scene)
+  const groundMaterial = new BABYLON.StandardMaterial('ground-material', scene)
   groundMaterial.diffuseColor = new BABYLON.Color3(0.462, 0.76, 0.404)
   ground.material = groundMaterial
 
-  const camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 4, Math.PI / 180 * 70, 40, BABYLON.Vector3.Zero(), scene)
+  const camera = new BABYLON.ArcRotateCamera(
+      'Camera',
+      Math.PI / 4,
+      Math.PI / 180 * 70,
+      40,
+      BABYLON.Vector3.Zero(),
+      scene
+  )
   camera.attachControl(canvas, true)
 
   scene.createDefaultLight(true)
