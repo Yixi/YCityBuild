bbfafc8182080fc60fe1137d0f326dcc24a15fc5
Yixi <lyh1112@gmail.com>
2020-01-08 23:28:49 +0800

    add t cross road

--- src/model/road.ts
+++ src/model/road.ts
@@ -9,14 +9,10 @@ export enum ROAD_TYPE {
 
 export const ROAD_NAME = 'road'
 
-export const createRoad = (scene: BABYLON.Scene, x: number, z: number, type: ROAD_TYPE = ROAD_TYPE.HORIZONTAL) => {
-
-  const shoulderMaterials = new BABYLON.StandardMaterial('shoulder', scene)
-  shoulderMaterials.diffuseColor = new BABYLON.Color3(0.83, 0.79, 0.77)
-
-  const roadBedMaterials = new BABYLON.StandardMaterial('roadBed', scene)
-  roadBedMaterials.diffuseColor = new BABYLON.Color3(0.43, 0.42, 0.44)
-
+const createHorizontalRoad = (
+    scene: BABYLON.Scene,
+    [shoulderMaterials, roadBedMaterials]: [BABYLON.StandardMaterial, BABYLON.StandardMaterial]
+) => {
   const shoulder = BABYLON.MeshBuilder.CreateBox(
       'shoulder',
       {
@@ -40,9 +36,9 @@ export const createRoad = (scene: BABYLON.Scene, x: number, z: number, type: ROA
   roadBed.material = roadBedMaterials
 
   shoulder.position = new BABYLON.Vector3(0, 0.01, -0.45)
-  rightShoulder.position = new BABYLON.Vector3(0, 0.01,  0.45)
+  rightShoulder.position = new BABYLON.Vector3(0, 0.01, 0.45)
 
-  roadBed.position = new BABYLON.Vector3(0, 0.001,  0)
+  roadBed.position = new BABYLON.Vector3(0, 0.001, 0)
   roadBed.rotation = new BABYLON.Vector3(Math.PI / 2, 0, 0)
 
   const road = BABYLON.Mesh.MergeMeshes(
@@ -54,11 +50,98 @@ export const createRoad = (scene: BABYLON.Scene, x: number, z: number, type: ROA
       true
   )
   road.name = ROAD_NAME
-  road.position= new BABYLON.Vector3(x, 0, z)
+  return road
+}
 
-  if (type === ROAD_TYPE.VERTICAL) {
-    road.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0)
+const createTIntersectionRoad = (
+    scene: BABYLON.Scene,
+    [shoulderMaterials, roadBedMaterials]: [BABYLON.StandardMaterial, BABYLON.StandardMaterial]
+) => {
+  const shoulder = BABYLON.MeshBuilder.CreateBox(
+      'shoulder',
+      {
+        width: 1,
+        height: 0.02,
+        depth: 0.1,
+      },
+      scene
+  )
+  shoulder.material = shoulderMaterials
+
+  const shoulderConner = BABYLON.MeshBuilder.CreateBox(
+      'shoulderConner',
+      {
+        width: 0.1,
+        height: 0.02,
+        depth: 0.1,
+      },
+      scene
+  )
+  shoulderConner.material = shoulderMaterials
+
+  const roadBed = BABYLON.MeshBuilder.CreatePlane(
+      'roadBed',
+      {
+        width: 0.9,
+        height: 0.9,
+      },
+      scene
+  )
+  roadBed.material = roadBedMaterials
+
+  shoulder.position = new BABYLON.Vector3(0, 0.01, -0.45)
+  const topShoulder = shoulder.clone()
+  topShoulder.rotation.y = Math.PI / 2
+  topShoulder.position = new BABYLON.Vector3(-0.45, 0.01, 0)
+  shoulderConner.position = new BABYLON.Vector3(0.45, 0.01, 0.45)
+
+  roadBed.position = new BABYLON.Vector3(0.05, 0.001, 0.05)
+  roadBed.rotation.x = Math.PI / 2
+
+  const road = BABYLON.Mesh.MergeMeshes(
+      [shoulder, topShoulder, roadBed, shoulderConner],
+      true,
+      true,
+      undefined,
+      false,
+      true
+  )
+  road.name = ROAD_NAME
+  return road
+}
+
+export const createRoad = (
+    scene: BABYLON.Scene,
+    x: number,
+    z: number,
+    type: ROAD_TYPE = ROAD_TYPE.HORIZONTAL,
+    rotate: number = 0
+) => {
+
+  const shoulderMaterials = new BABYLON.StandardMaterial('shoulder', scene)
+  shoulderMaterials.diffuseColor = new BABYLON.Color3(0.83, 0.79, 0.77)
+
+  const roadBedMaterials = new BABYLON.StandardMaterial('roadBed', scene)
+  roadBedMaterials.diffuseColor = new BABYLON.Color3(0.43, 0.42, 0.44)
+
+  let road: BABYLON.Mesh
+
+  if ([ROAD_TYPE.HORIZONTAL, ROAD_TYPE.VERTICAL].includes(type)) {
+    road = createHorizontalRoad(scene, [shoulderMaterials, roadBedMaterials])
+    if (type === ROAD_TYPE.VERTICAL) {
+      road.rotation.y = Math.PI / 2
+    }
   }
+
+  if (ROAD_TYPE.T_INTERSECTION === type) {
+    road = createTIntersectionRoad(scene, [shoulderMaterials, roadBedMaterials])
+  }
+
+  if (rotate) {
+    road.rotation.y = rotate
+  }
+
+  road.position = new BABYLON.Vector3(x, 0, z)
 
   return road
 }
--- src/road.ts
+++ src/road.ts
@@ -4,6 +4,14 @@ import { store } from '@root/data/store'
 
 export const buildRoads = (scene: BABYLON.Scene) => {
 
+  const t = createRoad(scene, 46, 50, ROAD_TYPE.T_INTERSECTION)
+  store.map[46][50].info.type = ROAD_TYPE.T_INTERSECTION
+  store.map[46][50].mesh = t
+
+  const t2 = createRoad(scene, 46, 53, ROAD_TYPE.T_INTERSECTION, Math.PI)
+  store.map[46][53].info.type = ROAD_TYPE.T_INTERSECTION
+  store.map[46][53].mesh = t
+
   store.road.forEach(([x, z]) => {
     const road = createRoad(scene, x, z)
     store.map[x][z].info.type = ROAD_TYPE.HORIZONTAL
@@ -13,11 +21,10 @@ export const buildRoads = (scene: BABYLON.Scene) => {
         store.map[x][z + 1].mesh?.name === ROAD_NAME ||
         store.map[x][z - 1].mesh?.name === ROAD_NAME
     ) {
-      console.log(x, z)
       scene.removeMesh(store.map[x][z].mesh)
       store.map[x][z].mesh = createRoad(scene, x, z, ROAD_TYPE.VERTICAL)
       store.map[x][z].info.type = ROAD_TYPE.VERTICAL
     }
-
   })
+
 }
